dde735fefe4e831a340bce18d5ebf7c7b6034d09
roguemammalstudent <135566059+roguemammalstudent@users.noreply.github.com>
2025-04-22 17:45:21 +1000

    Installed Dialogue System

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/SequencerMessageBehaviourDrawer.cs
@@ -0,0 +1,20 @@
+// Recompile at 22/04/2025 2:55:23 PM
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+using UnityEditor;
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [CustomPropertyDrawer(typeof(SequencerMessageBehaviour))]
+    public class SequencerMessageBehaviourDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("message"));
+        }
+    }
+}
+#endif
+#endif
