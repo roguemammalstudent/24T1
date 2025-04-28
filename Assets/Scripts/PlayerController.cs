d3ef134e4c86bcf67aca0b241ed0f43865ff09be
roguemammalstudent <135566059+roguemammalstudent@users.noreply.github.com>
2025-04-28 17:23:40 +1000

    Big Update
    New dialogue system, new dialogue, new animations, new sprites etc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,6 +36,8 @@ public class PlayerController : MonoBehaviour
     public AudioSource AudioSource;
     public SoundManager soundManager;
 
+    private Animator animator;
+
     //Variable for flipping the Player sprite
     private bool isFacingRight = true;
 
@@ -281,10 +283,10 @@ public class PlayerController : MonoBehaviour
         // Move the Player horizontally based on input
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         // Player presses the jump button and is grounded = jump
-        //  if (Input.GetButtonDown("Jump") && IsGrounded())
-        //  {
-        //      rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        //  }
+          if (Input.GetButtonDown("Jump") && isGrounded())
+          {
+              rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+          }
 
         // Player releases the jump button while still jumping = reduce jump height
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -934,11 +936,11 @@ public class PlayerController : MonoBehaviour
 
 
     //// Check if the player is on the ground
-    //private bool isGrounded()
-    //{
+    private bool isGrounded()
+    {
     //   // Use a Circle Cast to detect if there is ground beneath the Player
-    //   return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    //}
+       return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
 
     // Flip the player sprite if moving in the opposite direction
     private void Flip()
